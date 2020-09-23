3e7dd0986a65e149e592108efb0f9371a28905e3
Zoltan Altfatter <altfatterz@gmail.com>
2020-09-23 17:55:29 +0200

    creating json standard output

--- counter-maven-plugin/src/main/java/com/example/maven/plugin/DependencyCounterMojo.java
+++ counter-maven-plugin/src/main/java/com/example/maven/plugin/DependencyCounterMojo.java
@@ -1,20 +1,19 @@
 package com.example.maven.plugin;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.simpleflatmapper.csv.CsvParser;
-import org.simpleflatmapper.lightningcsv.CsvReader;
-import org.simpleflatmapper.util.CloseableIterator;
+import org.simpleflatmapper.lightningcsv.CloseableCsvReader;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 @Mojo(name = "dependency-counter", defaultPhase = LifecyclePhase.COMPILE)
@@ -38,14 +37,27 @@ public class DependencyCounterMojo extends AbstractMojo {
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     MavenProject project;
 
-    public void execute()  {
+    public void execute() {
         System.out.println("input:" + input.getAbsolutePath());
         System.out.println("output:" + output.getAbsolutePath());
 
-        try (CloseableIterator<String[]> it = CsvParser.iterator(input)) {
-            while(it.hasNext()) {
-                System.out.println(Arrays.toString(it.next()));
+        try {
+            CloseableCsvReader reader = CsvParser.reader(input);
+            JsonFactory jsonFactory = new JsonFactory();
+
+            Iterator<String[]> iterator = reader.iterator();
+            String[] headers = iterator.next();
+
+            try (JsonGenerator jsonGenerator = jsonFactory.createGenerator(System.out)) {
+                jsonGenerator.writeStartObject();
+                while (iterator.hasNext()) {
+                    String[] values = iterator.next();
+                    jsonGenerator.writeFieldName(values[0]);
+                    jsonGenerator.writeString(values[1]);
+                }
+                jsonGenerator.writeEndObject();
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
