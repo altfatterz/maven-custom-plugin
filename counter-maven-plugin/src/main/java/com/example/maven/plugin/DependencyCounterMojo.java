17011caa73fe29cfd2f0f645f998a983b96c1406
Zoltan Altfatter <altfatterz@gmail.com>
2020-09-23 22:10:55 +0200

    kind of working

--- counter-maven-plugin/src/main/java/com/example/maven/plugin/DependencyCounterMojo.java
+++ counter-maven-plugin/src/main/java/com/example/maven/plugin/DependencyCounterMojo.java
@@ -2,73 +2,86 @@ package com.example.maven.plugin;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
-import org.apache.maven.model.Dependency;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
 import org.simpleflatmapper.csv.CsvParser;
 import org.simpleflatmapper.lightningcsv.CloseableCsvReader;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Iterator;
-import java.util.List;
 
-@Mojo(name = "dependency-counter", defaultPhase = LifecyclePhase.COMPILE)
+@Mojo(name = "dependency-counter", defaultPhase = LifecyclePhase.PROCESS_RESOURCES)
 public class DependencyCounterMojo extends AbstractMojo {
 
-    /**
-     * Scope to filter the dependencies.
-     */
-    @Parameter(property = "scope")
-    String scope;
-
-    @Parameter(defaultValue = "src/main/resources/i18n/translations.csv", required = true)
+    @Parameter(defaultValue = "src/main/resources/static/i18n/translations.csv", required = true)
     private File input;
 
-    @Parameter(defaultValue = "public/i18n", required = true)
+    @Parameter(defaultValue = "target/classes/static/i18n", required = true)
     private File output;
 
-    /**
-     * Gives access to the Maven project information.
-     */
-    @Parameter(defaultValue = "${project}", required = true, readonly = true)
-    MavenProject project;
-
     public void execute() {
-        System.out.println("input:" + input.getAbsolutePath());
-        System.out.println("output:" + output.getAbsolutePath());
+        getLog().info("input:" + input.getAbsolutePath());
+        getLog().info("output:" + output.getAbsolutePath());
+        getLog().info("Generating translation files...");
 
         try {
+
             CloseableCsvReader reader = CsvParser.reader(input);
             JsonFactory jsonFactory = new JsonFactory();
 
             Iterator<String[]> iterator = reader.iterator();
             String[] headers = iterator.next();
 
-            try (JsonGenerator jsonGenerator = jsonFactory.createGenerator(System.out)) {
-                jsonGenerator.writeStartObject();
-                while (iterator.hasNext()) {
-                    String[] values = iterator.next();
-                    jsonGenerator.writeFieldName(values[0]);
-                    jsonGenerator.writeString(values[1]);
-                }
-                jsonGenerator.writeEndObject();
+            try (JsonGenerator jgEN = createJsonGenerator("en", jsonFactory);
+                 JsonGenerator jgDE = createJsonGenerator("de", jsonFactory);
+                 JsonGenerator jgFR = createJsonGenerator("fr", jsonFactory);
+                 JsonGenerator jgIT = createJsonGenerator("it", jsonFactory)) {
+
+                writeContent(jgEN, iterator, 2);
+                writeContent(jgDE, iterator, 3);
+                writeContent(jgFR, iterator, 4);
+                writeContent(jgIT, iterator, 5);
             }
+
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        List<Dependency> dependencies = project.getDependencies();
+    void writeContent(JsonGenerator jsonGenerator, Iterator<String[]> iterator, int column) throws IOException {
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeRaw('\n');
+        while (iterator.hasNext()) {
+            String[] values = iterator.next();
+            getLog().info("values:");
+            for (String value : values) {
+                getLog().info(value);
+            }
+            jsonGenerator.writeFieldName(values[0]);
+            jsonGenerator.writeString(values[column]);
+        }
+        jsonGenerator.writeRaw('\n');
+        jsonGenerator.writeEndObject();
+    }
 
-        long numDependencies = dependencies.stream()
-                .filter(d -> (scope == null || scope.isEmpty()) || scope.equals(d.getScope()))
-                .count();
+    Path createTranslationFile(String language) throws IOException {
+        Path path = Paths.get(output + "/locale-" + language + ".json");
+        System.out.println("path:" + path);
+        // Files.deleteIfExists(path);
+        return Files.createFile(path);
+    }
 
-        getLog().info("Number of dependencies: " + numDependencies);
+    JsonGenerator createJsonGenerator(String language, JsonFactory jsonFactory) throws IOException {
+        return jsonFactory.createGenerator(Files.newOutputStream(createTranslationFile(language)))
+                .setPrettyPrinter(new DefaultPrettyPrinter());
     }
 
 }
