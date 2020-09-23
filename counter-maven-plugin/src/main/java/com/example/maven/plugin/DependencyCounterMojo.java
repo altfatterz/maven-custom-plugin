aabaacc08b8de04816166f1526fd8b395efc3659
Zoltan Altfatter <altfatterz@gmail.com>
2020-09-23 17:13:25 +0200

    initial working version

---
+++ counter-maven-plugin/src/main/java/com/example/maven/plugin/DependencyCounterMojo.java
@@ -0,0 +1,37 @@
+package com.example.maven.plugin;
+
+import org.apache.maven.model.Dependency;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.util.List;
+
+@Mojo(name = "dependency-counter", defaultPhase = LifecyclePhase.COMPILE)
+public class DependencyCounterMojo extends AbstractMojo {
+
+    /**
+     * Scope to filter the dependencies.
+     */
+    @Parameter(property = "scope")
+    String scope;
+
+    /**
+     * Gives access to the Maven project information.
+     */
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    MavenProject project;
+
+    public void execute() {
+        List<Dependency> dependencies = project.getDependencies();
+
+        long numDependencies = dependencies.stream()
+                .filter(d -> (scope == null || scope.isEmpty()) || scope.equals(d.getScope()))
+                .count();
+
+        getLog().info("Number of dependencies: " + numDependencies);
+    }
+
+}
