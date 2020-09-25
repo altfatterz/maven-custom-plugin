c4248f58732d287d7ec49b667faf38ee3a29d109
Zoltan Altfatter <altfatterz@gmail.com>
2020-09-25 17:39:29 +0200

    initial Kotlin module

---
+++ translation-files-generator-kotlin/src/main/kotlin/com/example/maven/plugin/Translator.kt
@@ -0,0 +1,7 @@
+package com.example.maven.plugin
+
+class Translator {
+
+
+
+}
---
+++ translation-files-generator-kotlin/src/test/kotlin/com/example/maven/plugin/TranslatorTest.kt
@@ -0,0 +1,5 @@
+package kotlin.com.example.maven.plugin
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class TranslatorTest
